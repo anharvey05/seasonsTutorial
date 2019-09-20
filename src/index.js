02b1878f3094520d340a40cdca7f147426f12d60
anharvey05 <amy.harvey@onshoreoutsourcing.com>
2019-09-20 15:44:07 -0500

    Through lesson 54

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
+class App extends React.Component {
+    constructor (props) {
+        super(props);
 
-    return (
-        <div>
-            <SeasonDisplay />
-        </div>
-    );  
-};
+        //This is the only time we do direct assignment to this.state
+        this.state = { lat: 40, long: 40 };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+                this.setState({long: position.coords.longitude})
+            },
+            (err) => console.log(err)
+        );
+    }
+
+    // React says we have to define render!!
+    render () {
+        return (
+            <div>
+                <div>Latitude: {this.state.lat}</div>
+                <div>Longitude: {this.state.long}</div>
+            </div>
+        )
+        
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
